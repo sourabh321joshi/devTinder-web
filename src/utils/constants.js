827a519a6497569b0f00fdf3891b630d50d94d8e
sourabh321joshi <162778454+sourabh321joshi@users.noreply.github.com>
2025-12-21 11:30:15 +0530

    Update BASE_URL for development environment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,9 @@
 
 
 
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" :"/api"
+//export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" :"/api"
+
+export const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000"
+    : "https://devtinder-7iaj.onrender.com";
